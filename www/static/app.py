bff6b93df6c763275d0043a7e447383095933a9c
tony <szhujiabin@163.com>
2016-01-08 00:03:56 +0800

    updat

--- www/static/app.py
+++ www/static/app.py
@@ -1,20 +1,28 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*-
+
 import logging;logging.basicConfig(level=logging.INFO)
 
-import asynico,os,json,time
+import asyncio,os,json,time
 from datetime import datetime
 from aiohttp import web
 
-def index(request):
+def handle(request):
+	name = request.match_info.get('name', "Anonymous")
+	logging.info("handle a request..."+name)
 	return web.Response(body=b'<h1>Awesome</h1>')
 
-@asynico.coruntine
+@asyncio.coroutine
 def init(loop):
 	app = web.Application(loop=loop)
-	app.router.add.route('GET','/',index)
+	app.router.add_route('GET','/{name}',handle)
 	srv = yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
 	logging.info("server started at http://127.0.0.1:9000..")
 	return srv
 
-loop = asynico.get_event_loop()
+loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
-loop.run_forver()
+try:
+	loop.run_forever()
+except KeyboardInterrupt:
+	pass
